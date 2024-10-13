b0015a6e4d6bda11512b075bb8f9ed9a5c69202d
Shiven Singh <ss.shiven44@gmail.com>
2024-10-13 12:57:46 +0100

    Update more for loop concepts.cpp

--- more for loop concepts.cpp
+++ more for loop concepts.cpp
@@ -32,3 +32,80 @@ int main(){
 3 4
 3 5
 4 4
+
+ #include<iostream>
+using namespace std;
+int main(){
+    for(int i=0; i<5;i++)
+    {
+    for(int j=1; j<=5; j++){
+        cout<<i<< " "<<j<<endl;
+    }
+    }
+}
+//Output
+0 1
+0 2
+0 3
+0 4
+0 5
+1 1
+1 2
+1 3
+1 4
+1 5
+2 1
+2 2
+2 3
+2 4
+2 5
+3 1
+3 2
+3 3
+3 4
+3 5
+4 1
+4 2
+4 3
+4 4
+4 5
+
+    
+#include<iostream>
+using namespace std;
+int main(){
+    for(int i=0; i<=15;i+=2)
+    {
+       cout<<i<<" ";
+    if( i&1){
+        continue;
+    }
+    i++;
+    }
+}
+
+//Output
+0 3 5 7 9 11 13 15 
+    
+#include<iostream>
+using namespace std;
+int main(){
+    for(int i=0; i<=15;i--)
+    {
+        cout<<i;
+        i++;
+    }
+}
+//Output
+Infinite Zero loop
+
+#include<iostream>
+using namespace std;
+int main(){
+    for(int i=0; i<=5;i++){
+        cout<<i;
+        i++;
+    }
+}
+//Output
+0 2 4
