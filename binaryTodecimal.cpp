612a3800e7de7f9b69d9a2321287168ca3d9bf84
Shiven Singh <ss.shiven44@gmail.com>
2024-10-21 13:13:36 +0100

    lesson_6

--- binaryTodecimal.cpp
+++ binaryTodecimal.cpp
@@ -2,25 +2,29 @@
 #include<math.h>
 using namespace std;
 int main(){
-    int n;
+
+int i =0;
+int n;
 cin>>n;
-int i=0;
-// int n;
- int ans=0;
+int ans = 0;
 while(n!=0){
-int bit= n & 1;
-  ans=(bit * pow(10,i))+ans;
-  n=n>>1;
-  i++;
-
-  
+    int digit = n%10;
+    if(digit==1){
+        ans = ans + pow(2,i);
+    }
+    n=n/10;
+    i++;
 }
-cout<<ans<<endl;
+cout<<ans;
 
 
 
 
 
 
-  
+
+
+
+
+    return 0;
 }
--- decimalTObinary.cpp
+++ decimalTObinary.cpp
@@ -2,29 +2,25 @@
 #include<math.h>
 using namespace std;
 int main(){
-
-int i =0;
-int n;
+    int n;
 cin>>n;
-int ans = 0;
+int i=0;
+// int n;
+ int ans=0;
 while(n!=0){
-    int digit = n%10;
-    if(digit==1){
-        ans = ans + pow(2,i);
-    }
-    n=n/10;
-    i++;
+int bit= n & 1;
+  ans=(bit * pow(10,i))+ans;
+  n=n>>1;
+  i++;
+
+  
 }
-cout<<ans;
+cout<<ans<<endl;
 
 
 
 
 
 
-
-
-
-
-    return 0;
+  
 }
