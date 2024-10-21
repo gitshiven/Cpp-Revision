9692bb1cad2a634b88aad33118069b7e76a17c61
Shiven Singh <ss.shiven44@gmail.com>
2024-10-21 15:43:46 +0100

    lesson_7.1

---
+++ complement_base_10_ integer.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+using namespace std;
+class Solution {
+public:
+    int bitwiseComplement(int n) {
+        int m=n;
+        int mask = 0;
+        if(n==0){
+            return 1;
+        }
+        while(m!=0) {
+            mask = (mask << 1) | 1;
+            m=m>>1; 
+         } 
+         int ans = (~n)&mask;
+        return ans;
+    }
+};
+int main() {
+    Solution solution;
+    int n;
+    cout << "Enter a number: ";
+    cin >> n;
+    int result = solution.bitwiseComplement(n);
+    cout << "Bitwise complement of " << n << " is: " << result << endl;
+    return 0;
+}
