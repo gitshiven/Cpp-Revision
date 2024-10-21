9a918e4e351b05977dc969c866166f1a57e4d6e6
Shiven Singh <ss.shiven44@gmail.com>
2024-10-21 15:16:56 +0100

    final_code

--- reverse integer.cpp
+++ reverse integer.cpp
@@ -2,21 +2,18 @@
 #include<math.h>
 using namespace std;
 int main(){
-    // int x; 
-    //     int i=0;
-    //     int ans=0;
-    //     cin>>x;
-    //     while(x!=0){
-    //         int digit = x%10;
-    //         // if((ans>INT_MAX/10) ||(ans<INT_MIN/10)){
-    //         // cout<< 0;
-    //         // }
-    //         ans=digit+(10*ans);
-    //         x=x/10;
-    //         i++;
-    // }
-    // cout<<ans;
     int x;
-    cin>>x;
-    cout<<(~x);
+        int i=0;
+        int ans=0;
+        cin>>x;
+        while(x!=0){
+            int digit = x%10;
+            // if((ans>INT_MAX/10) ||(ans<INT_MIN/10)){
+            // cout<< 0;
+            // }
+            ans=digit+(10*ans);
+            x=x/10;
+            i++;
+    }
+    cout<<ans;
 };
